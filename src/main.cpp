1fb23f982125aeb70480cb8c297e0f8d3f82b7c0
Squinky86 <squinky86@gmail.com>
2020-01-16 08:16:12 -0600

    more STIGing

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,17 @@ int main(int argc, char *argv[])
     {
         int onTest = 0;
 
+        //general information
+        {
+            std::cout << "Running Tests in " << QDir::currentPath().toStdString() << std::endl;
+            QDirIterator it(QDir::currentPath());
+            while (it.hasNext())
+            {
+                QFile f(it.next());
+                std::cout << f.fileName().toStdString() << std::endl;
+            }
+        }
+
         //run tests
         IgnoreWarnings = true;
 
