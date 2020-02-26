6d80949cb39f3ce15439bd30bd29f9fbb1fb81cf
Squinky86 <squinky86@gmail.com>
2020-02-26 08:01:18 -0600

    out-of-cycle update to quarterly

--- src/dbmanager.cpp
+++ src/dbmanager.cpp
@@ -2685,7 +2685,7 @@ bool DbManager::UpdateDatabaseFromVersion(int version)
             q.bindValue(QStringLiteral(":value"), QStringLiteral("n"));
             ret = q.exec() && ret;
             q.bindValue(QStringLiteral(":name"), QStringLiteral("quarterly"));
-            q.bindValue(QStringLiteral(":value"), QStringLiteral("https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_SRG-STIG_Library_2020_01.zip"));
+            q.bindValue(QStringLiteral(":value"), QStringLiteral("https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_SRG-STIG_Library_2020_01v4.zip"));
             ret = q.exec() && ret;
             ret = UpdateVariable(QStringLiteral("version"), QStringLiteral("2")) && ret;
         }
